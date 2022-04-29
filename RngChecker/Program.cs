b5ea18eb9fe9ba8e84605a8cb938c176ebea5ff8
Hourier <66951241+Hourier@users.noreply.github.com>
2022-04-29 20:08:37 +0900

    [Implement] Made Random Number Generator Checker

---
+++ RngChecker/Program.cs
@@ -0,0 +1,11 @@
+﻿using MathNet.Numerics.Statistics;
+using RandomChecker;
+
+const double inputAverage = 5;
+const double inputDeviation = 1;
+const int dataCount = 10000000;
+var rngProcessor = new RandomNumbersProcessor(inputAverage, inputDeviation);
+rngProcessor.StartTimer();
+rngProcessor.Generate(dataCount);
+rngProcessor.StopTimer();
+rngProcessor.MakeResult();
